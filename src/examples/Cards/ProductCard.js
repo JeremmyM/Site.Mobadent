ab4421e83966802939ddd8388c9e3fddfe793196
JeremmyMolina <cryptomoli16@gmail.com>
2025-09-05 02:37:51 +0500

    Tarjetas de productos

--- src/examples/Cards/ProductCard.js
+++ src/examples/Cards/ProductCard.js
@@ -1,105 +1,55 @@
 // src/examples/Cards/ProductCard.js
 
-import React from "react";
-import PropTypes from "prop-types";
-
-// Importa los componentes de Material Kit
+// Importa los componentes de Material Kit y PropTypes
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import MKButton from "components/MKButton";
+import PropTypes from "prop-types";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Divider from "@mui/material/Divider";
 
 function ProductCard({ image, name, description, onAddToCart }) {
   return (
-    <MKBox
-      borderRadius="xl"
-      shadow="xl"
-      p={2}
-      mb={3}
-      sx={{
-        overflow: "hidden",
-        backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        textAlign: "center",
-        minHeight: "450px",
-        justifyContent: "space-between",
-      }}
-    >
-      <MKBox
+    <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+      <CardMedia
+        component="img"
+        image={image}
+        title={name}
         sx={{
-          width: "100%",
-          height: "200px", // Altura fija para el contenedor de la imagen
-          mb: 2,
-          display: "flex", // Para centrar la imagen
-          justifyContent: "center",
-          alignItems: "center",
+          height: 200,
+          // Cambiamos 'cover' a 'contain' para que la imagen no se recorte
+          objectFit: "contain",
+          borderTopLeftRadius: ({ borders }) => borders.borderRadius.lg,
+          borderTopRightRadius: ({ borders }) => borders.borderRadius.lg,
         }}
-      >
-        <MKBox
-          component="img"
-          src={image}
-          alt={name}
-          borderRadius="lg"
-          sx={{
-            width: "100%",
-            maxHeight: "100%",
-            // **** CAMBIO CLAVE AQUI ****
-            objectFit: "contain",
-          }}
-        />
-      </MKBox>
-      <MKBox
-        sx={{
-          height: "120px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100%",
-          justifyContent: "flex-start",
-        }}
-      >
-        <MKTypography
-          variant="h5"
-          color="dark"
-          fontWeight="bold"
-          mb={1}
-          sx={{
-            minHeight: "40px",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: "vertical",
-          }}
-        >
+      />
+      <MKBox p={2} sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+        <MKTypography variant="h5" fontWeight="bold" textTransform="capitalize" gutterBottom>
           {name}
         </MKTypography>
         <MKTypography
           variant="body2"
           color="text"
-          my={1}
           sx={{
-            minHeight: "60px",
-            maxHeight: "90px",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: "vertical",
+            flexGrow: 1,
+            display: "block",
+            lineHeight: "1.5",
           }}
         >
           {description}
         </MKTypography>
+        <Divider sx={{ my: 1.5 }} />
+        <MKButton variant="gradient" color="info" onClick={onAddToCart} fullWidth>
+          Ver Detalles
+        </MKButton>
       </MKBox>
-      <MKButton variant="gradient" color="primary" sx={{ mt: "auto" }} onClick={onAddToCart}>
-        Ver Detalles 👁️
-      </MKButton>
-    </MKBox>
+    </Card>
   );
 }
 
-// Validación de las propiedades (props) del componente
 ProductCard.propTypes = {
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
